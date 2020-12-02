2e3251fb4c41523a7350a51d5274982117620d5f
Sammy970 <70690987+Sammy970@users.noreply.github.com>
2020-12-02 20:20:33 +0530

    Update fastgcp.sh

--- fastgcp.sh
+++ fastgcp.sh
@@ -10,16 +10,16 @@ echo "  ###   ###   ########## ########## ########   ########  ###       ### ###
 echo ""
 
 sudo apt-get update
-/n
+\n
 sudo apt-get upgrade -y
-/n
+\n
 
 cd
 mkdir bin
 cd bin
 curl http://commondatastorage.googleapis.com/git-repo-downloads/repo > ~/bin/repo 
 chmod a+x ~/bin/repo
-/n
+\n
 sudo apt-get install bison build-essential curl ccache flex lib32ncurses5-dev lib32z1-dev libesd0-dev libncurses5-dev libsdl1.2-dev libxml2 libxml2-utils lzop pngcrush schedtool squashfs-tools xsltproc zip zlib1g-dev git-core make phablet-tools gperf openjdk-8-jdk -y
 
 git clone https://github.com/akhilnarang/scripts.git build
