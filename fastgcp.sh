924747f8a384f225b4f2d34a217ee6f6dc59d37d
Sammy970 <70690987+Sammy970@users.noreply.github.com>
2020-12-02 22:06:51 +0530

    Update fastgcp.sh

--- fastgcp.sh
+++ fastgcp.sh
@@ -30,5 +30,5 @@ git clone https://github.com/PixelExtended/device_xiaomi_jasmine_sprout.git devi
 git clone https://github.com/PixelExtended/device_xiaomi_sdm660-common.git device/xiaomi/sdm660-common '\n'
 git clone https://github.com/xiaomi-sdm660/android_vendor_xiaomi_sdm660-common.git vendor/xiaomi/sdm660-common '\n'
 git clone https://github.com/xiaomi-sdm660/android_kernel_xiaomi_sdm660.git kernel/xiaomi/sdm660 '\n'
-
+git clone https://github.com/xiaomi-sdm660/vendor_xiaomi_wayne-common.git vendor/xiaomi/wayne-common
 echo " Thankyou"
