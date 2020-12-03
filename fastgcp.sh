3132be3ee036d6a3b3ecee16b6996d5660d54ad1
Sammy970 <70690987+Sammy970@users.noreply.github.com>
2020-12-03 12:33:49 +0530

    Update fastgcp.sh

--- fastgcp.sh
+++ fastgcp.sh
@@ -15,6 +15,8 @@ echo "**************************************************************************
 cd
 sudo apt-get install bison build-essential curl ccache flex lib32ncurses5-dev lib32z1-dev libesd0-dev libncurses5-dev libsdl1.2-dev libxml2 libxml2-utils lzop pngcrush schedtool squashfs-tools xsltproc zip zlib1g-dev git-core make phablet-tools gperf openjdk-8-jdk -y
 echo "********************************************************************************************************************************************************"
+cd
+cd bin/
 git clone https://github.com/akhilnarang/scripts.git build
 cd build/setup/
 bash android_build_env.sh
@@ -24,6 +26,8 @@ cd bin/build/
 mkdir pex
 cd pex
 echo "********************************************************************************************************************************************************"
+cd
+cd bin/build/pex/
 git config --global user.name "Samyak"
 git config --global user.email "jainsamyak2002.sj@gmail.com"
 echo "********************************************************************************************************************************************************"
@@ -32,6 +36,8 @@ cd bin/build/pex
 repo init -u https://github.com/PixelExtended/manifest -b eleven --depth=1
 repo sync -c -q --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all)
 echo "********************************************************************************************************************************************************"
+cd
+cd bin/build/pex/
 git clone https://github.com/PixelExtended/device_xiaomi_jasmine_sprout.git device/xiaomi/jasmine_sprout 
 git clone https://github.com/PixelExtended/device_xiaomi_sdm660-common.git device/xiaomi/sdm660-common 
 git clone https://github.com/xiaomi-sdm660/android_vendor_xiaomi_sdm660-common.git vendor/xiaomi/sdm660-common 
